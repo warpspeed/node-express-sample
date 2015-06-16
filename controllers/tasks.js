24b1c467f9b778a4e3fff5e0bcdd0650e655b04e
Keenan Treat <keet@sas.upenn.edu>
2015-06-16 10:57:26 -0500

    Renamed Tasks model to Task

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -2,17 +2,17 @@ var express = require('express'),
 	ObjectID = require('mongodb').ObjectID;
 	router  = express.Router();
 
-var Tasks = require('../models/tasks');
+var Task = require('../models/Task');
 
 	router.get('/', function(req, res) {
-		Tasks.all(function(err, docs) {
+		Task.all(function(err, docs) {
 			res.render('index', {tasks: docs} );
 		});
 	});
 
 	router.post('/tasks/:id/toggle-complete', function(req, res) {
 		var id = new ObjectID(req.params.id);
-		Tasks.toggle(id, function(err, result) {
+		Task.toggleComplete(id, function(err, result) {
 			if(result) {
 				res.redirect('/');
 			} else {
@@ -25,7 +25,7 @@ var Tasks = require('../models/tasks');
 	});
 
 	router.post('/tasks/clear-complete', function(req, res) {
-		Tasks.clearComplete(function(err, result) {
+		Task.clearComplete(function(err, result) {
 			if(result) {
 				res.redirect('/');
 			} else {
@@ -36,7 +36,7 @@ var Tasks = require('../models/tasks');
 
 	router.post('/tasks' ,function(req, res) {
 		if(req.body.name) {
-			Tasks.create(req.body.name, function(err, result){
+			Task.create(req.body.name, function(err, result){
 				if(result) {
 					res.redirect('/');
 				} else {
