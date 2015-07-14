4be67250289aaf6c42c07c5b1cebe361c4bb5158
Chris Turner <chris@turnerlogic.com>
2015-07-14 14:53:02 -0500

    Added restart.

--- warpspeed.sh
+++ warpspeed.sh
@@ -1,4 +1,8 @@
 # warpspeed.sh
 # Commands here will be run each time a pull or push deploy is successfully run.
 
+# Install dependencies.
 npm install
+
+# Restart passenger.
+touch tmp/restart.txt
